4848e4eec48d235490d8570dd2d0ec3ae68dd062
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-02-22 19:25:24 +0100

    tailwind

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { StoryblokVue, apiPlugin } from "@storyblok/vue";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(StoryblokVue, {
+  accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
 
-app.mount('#app')
+  bridge: process.env.NODE_ENV !== "production", // optimizes by excluding the bridge on production
+
+  use: [apiPlugin],
+});
+
+app.use(router);
+
+app.mount("#app");
