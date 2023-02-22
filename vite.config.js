db33548cf829edb719c06b2084ebcae72ac12bf2
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-02-22 19:02:39 +0100

    https server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createServer } from "vite";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  server: {
+    https: {
+      key: "./localhost-key.pem",
+      cert: "./localhost.pem",
+    },
+  },
+  // Move createServer inside the defineConfig function
+  createServer: {
+    https: {
+      key: "./localhost-key.pem",
+      cert: "./localhost.pem",
+    },
+  },
+});
