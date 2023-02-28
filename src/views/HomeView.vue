45de5d13c0e4da9c2d55e7081a9d086b69db4f9c
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-02-28 13:31:46 +0100

    hero + font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { useStoryblok } from "@storyblok/vue";
 const story = await useStoryblok("home", { version: "draft" });
 </script>
 <template>
-  <div class="">
+  <main>
     <StoryblokComponent v-if="story" :blok="story.content" />
-  </div>
+  </main>
 </template>
