925b34ca1a415b5f249aedc3e30253aa030356f0
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-03-03 15:57:35 +0100

    infotext

--- src/components/infoText.vue
+++ src/components/infoText.vue
@@ -1,33 +1,59 @@
 <script setup>
-import Storyblok from "storyblok-js-client";
-import { computed } from "vue";
-const Api = new Storyblok();
+import { ref, computed, watchEffect } from "vue";
 
-const { text } = defineProps({ text: String });
-
-const richtext = computed(() => {
-  return text ? Api.richTextResolver.render(text) : "";
-});
-console.log(text);
-</script>
-<!-- <script setup>
 const { blok } = defineProps({ blok: Object });
-</script> -->
+
+const boldText = ref(false);
+
+const boldCheck = computed(() => {
+  let hasbold = false;
+  for (let content of blok.content.content) {
+    for (let text of content.content) {
+      let hasboldInner = false;
+      for (let marks of text.marks) {
+        if (marks.type === "bold") {
+          hasboldInner = true;
+          break;
+        }
+      }
+      if (hasboldInner) {
+        hasbold = true;
+        break;
+      }
+    }
+    if (hasbold) {
+      break;
+    }
+  }
+  return hasbold;
+});
+
+watchEffect(() => {
+  boldText.value = boldCheck.value;
+});
+</script>
+
 <template>
   <section v-editable="blok" class="w-screen bg-blauw-theme py-6">
     <div class="title container mx-auto">
       <div class="p-6">
         <h1 class="text-[35px] font-bold">{{ blok.title }}</h1>
       </div>
-      <div class="text p-6 leading-7">
-        <div v-html="richtext"></div>
-        <!-- <p v-for="content in tblok.content">
-          {{ content.text }}
-        </p> -->
+      <div class="text p-6 leading-7" v-for="content in blok.content.content">
+        <p>
+          <span
+            :class="{
+              'font-bold': text.marks && text.marks[0].type === 'bold',
+              italic: text.marks && text.marks[0].type === 'italic',
+            }"
+            v-for="text in content.content"
+            >{{ text.text }}</span
+          >
+        </p>
       </div>
       <div class="container mt-4 flex justify-center">
         <button
-          class="h-[62px] w-[260px] rounded-[86px] bg-orange-theme py-[11px] px-[76px] text-white transition hover:bg-hover-orange"
+          class="h-[62px] w-[260px] rounded-[86px] bg-orange-theme py-[11px] px-[76px] font-bold text-white transition hover:bg-hover-orange"
         >
           {{ blok.callToAction }}
         </button>
