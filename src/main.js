463e977f1f304d329dc1a58958638727744189c3
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-03-14 13:00:05 +0100

    enable storyblok bridge

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ app.component("Page", Page);
 
 app.use(StoryblokVue, {
   accessToken: "QWGzMMqBPxmbAWvxNNUf2Att",
-
+  bridge: true, // enable the bridge
   bridge: process.env.NODE_ENV !== "production", // optimizes by excluding the bridge on production
 
   use: [apiPlugin],
