55d9f97cc32ad5646d79e6f4dbbf3f99c2660d57
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-04-14 13:55:04 +0200

    cookies cookiebot

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProjectView from "../views/ProjectView.vue";
 import pageNotFoundView from "../views/pageNotFoundView.vue";
+import CookieView from "../views/CookieView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: "/project/:slug",
       name: "project",
       component: ProjectView,
+    },
+    {
+      path: "/cookie",
+      name: "cookie",
+      component: CookieView,
     },
     {
       path: "/:pathMatch(.*)*",
---
+++ src/views/CookieView.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "Cookiebot",
+  mounted() {
+    const script = document.createElement("script");
+    script.id = "CookieDeclaration";
+    script.src =
+      "https://consent.cookiebot.com/a971830f-b965-4fc6-a4b7-3e84567a5198/cd.js";
+    script.async = true;
+    document.body.appendChild(script);
+  },
+};
+</script>
+<template></template>
