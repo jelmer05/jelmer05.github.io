839505dc3a83eb101d469fd56a61ea51223762ef
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-04-17 19:51:13 +0200

    Responsive

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -3,15 +3,17 @@ const { blok } = defineProps({ blok: Object });
 </script>
 
 <template>
-  <section class="flex w-4/5 justify-between gap-3 self-center">
-    <div class="flex w-5/12 items-center">
+  <section
+    class="flex w-4/5 flex-col justify-between gap-3 self-center md:flex-row"
+  >
+    <div class="flex w-full items-center md:w-5/12">
       <StoryblokComponent
         v-for="blok in blok.Links"
         :blok="blok"
         :key="blok._uid"
       />
     </div>
-    <div class="w-5/12">
+    <div class="w-full md:w-5/12">
       <StoryblokComponent
         v-for="blok in blok.Rechts"
         :blok="blok"
