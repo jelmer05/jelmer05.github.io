7a1fc432bd4687b09680017cbd638efd88f48df0
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-04-22 16:17:22 +0200

    img blok whitebg optie

--- src/components/ImgBlok.vue
+++ src/components/ImgBlok.vue
@@ -2,7 +2,13 @@
 const { blok } = defineProps({ blok: Object });
 </script>
 <template>
-  <div>
-    <img class="h-3/6 w-[727px]" :src="blok.img.filename" :alt="blok.img.alt" />
+  <div :class="{ 'bg-white': blok.witbg }" class="w-full rounded-lg py-20">
+    <div class="container flex items-center justify-center rounded-lg">
+      <img
+        class="h-3/6 w-[727px]"
+        :src="blok.img.filename"
+        :alt="blok.img.alt"
+      />
+    </div>
   </div>
 </template>
