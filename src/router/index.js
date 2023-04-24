8efb701329e8b118f8c39abbea18891d48f7e54b
jelmer05 <114982452+jelmer05@users.noreply.github.com>
2023-04-24 11:57:01 +0200

    index fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import ProjectView from "../views/ProjectView.vue";
 import pageNotFoundView from "../views/pageNotFoundView.vue";
 import Solicitatie from "../views/LandingView.vue";
+import CookieView from "../views/CookieView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/cookie",
+      name: "cookie",
+      component: CookieView,
     },
     {
       path: "/project/",
