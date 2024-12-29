499c67dd4130900e4d5da2b7960235894feb9cd0
jelmer05 <jelmer05@users.noreply.github.com>
2024-12-29 01:26:34 +0000

    [create-pull-request] automated change

---
+++ node_modules/storyblok-js-client/tests/utils.ts
@@ -0,0 +1,7 @@
+export function headersToObject(headers: Headers) {
+  const obj: { [key: string]: string } = {};
+  for (const [key, value] of headers.entries()) {
+    obj[key] = value;
+  }
+  return obj;
+}
